e315ff0c9bbfb614fe15eed849d77f1fbe2adf69
kolamat <matanmidupe004@gmail.com>
2024-05-02 23:07:33 +0100

    with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/page.jsx

--- src/page.jsx
+++ src/page.jsx
@@ -1,14 +1,17 @@
 import { ReactTyped } from "react-typed";
 
 const MyComponent = () => (
-  <ReactTyped
-    startWhenVisible="true"
-    strings={[
-      "If <strong>startWhenVisible</strong> is <strong>true</strong>, will start when is visible in the dom",
-      "wetin dey sup bahi??",
-    ]}
-    typeSpeed={40}
-  />
+  <>
+    <ReactTyped
+      startWhenVisible="true"
+      strings={[
+        "If <strong>startWhenVisible</strong> is <strong>true</strong>, will start when is visible in the dom",
+        "wetin dey sup bahi??",
+      ]}
+      typeSpeed={40}
+    />
+    kolade
+  </>
 );
 
 export default MyComponent;
