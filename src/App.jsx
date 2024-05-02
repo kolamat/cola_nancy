ee40448958121495e456e4865ecb3bcf3e06048f
kolamat <matanmidupe004@gmail.com>
2024-05-02 22:00:53 +0100

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/App.jsx
    	new file:   src/page.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,15 @@
 import nancy from "/image.png";
-import { ReactTyped,Typed } from "react-typed";
+import typed from "./page";
 
-
-
-
-// const MyComponent = () => (
-//   <div>
-//     <ReactTyped strings={["Here you can find anything"]} typeSpeed={40} />
-//     <br />
-
-//     <ReactTyped
-//       strings={[
-//         "Search for products",
-//         "Search for categories",
-//         "Search for brands",
-//       ]}
-//       typeSpeed={40}
-//       backSpeed={50}
-//       attr="placeholder"
-//       loop
-//     >
-//       <input type="text" />
-//     </ReactTyped>
-//   </div>
-// );
-
-// const MyComponent  {
-    
-// }
+console.log(typed);
 
 function App() {
-    const [typed,setTyped] = React.useState<Typed| undefined>
-
-
   return (
     <>
       <div className={`flex justify-center my-auto mx-auto`}>
-        {MyComponent}
-        <img src={nancy} width={50} height={1} className="h-screen w-full" />
-        <div>
-          <Button onClick={typed.start()}>Start</Button>
-          <Button onClick={typed.stop()}>Stop</Button>
-          <Button onClick={typed.toggle()}>Toggle</Button>
-          <Button onClick={typed.destroy()}>Destroy</Button>
-          <Button onClick={typed.reset()}>Reset</Button>
-          <ReactTyped
-            typedRef={setTyped}
-            strings={["Here you can find anything"]}
-            typeSpeed={40}
-          />
-        </div>
+        <img src={nancy} width={50} height={1} className="h- w-" />
       </div>
+      <div>{typed}</div>
     </>
   );
 }
---
+++ src/page.jsx
@@ -0,0 +1,14 @@
+import { ReactTyped } from "react-typed";
+
+const MyComponent = () => (
+  <ReactTyped
+    startWhenVisible="true"
+    strings={[
+      "If <strong>startWhenVisible</strong> is <strong>true</strong>, will start when is visible in the dom",
+      "wetin dey sup bahi??",
+    ]}
+    typeSpeed={40}
+  />
+);
+
+export default MyComponent;
