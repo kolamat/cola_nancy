9db737943f3e8ab096b618e2910bf356425df30e
kolamat <matanmidupe004@gmail.com>
2024-05-02 21:32:47 +0100

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,56 @@
 import nancy from "/image.png";
-import Type from "typed-js";
+import { ReactTyped } from "react-typed";
+import { ReactTyped,Typed } from "react-typed";
 
-class User extends Type {
-  constructor(name, password) {
-    super({
-      username: Type.STRING,
-      password: Type.STRING,
-    });
-    this.username = name;
-    this.password = password;
-  }
-  getUserName() {
-    return this.username;
-  }
-  setUserName(kolade) {
-    this.username = kolade;
-  }
+
+
+
+// const MyComponent = () => (
+//   <div>
+//     <ReactTyped strings={["Here you can find anything"]} typeSpeed={40} />
+//     <br />
+
+//     <ReactTyped
+//       strings={[
+//         "Search for products",
+//         "Search for categories",
+//         "Search for brands",
+//       ]}
+//       typeSpeed={40}
+//       backSpeed={50}
+//       attr="placeholder"
+//       loop
+//     >
+//       <input type="text" />
+//     </ReactTyped>
+//   </div>
+// );
+
+const MyComponent  {
+    const [typed,setTyped] = React.useState<Typed| undefined>()
 }
 
-console.log(User);
-
 function App() {
+
+
+
   return (
     <>
       <div className={`flex justify-center my-auto mx-auto`}>
+        {MyComponent}
         <img src={nancy} width={50} height={1} className="h-screen w-full" />
+        <div>
+          <Button onClick={typed.start()}>Start</Button>
+          <Button onClick={typed.stop()}>Stop</Button>
+          <Button onClick={typed.toggle()}>Toggle</Button>
+          <Button onClick={typed.destroy()}>Destroy</Button>
+          <Button onClick={typed.reset()}>Reset</Button>
+          <ReactTyped
+            typedRef={setTyped}
+            strings={["Here you can find anything"]}
+            typeSpeed={40}
+          />
+        </div>
       </div>
     </>
   );
