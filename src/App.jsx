199ae7ada70475d212efa0b7e31c3c4be3095dbf
kolamat <matanmidupe004@gmail.com>
2024-05-02 20:44:22 +0100

    Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className={`text-5xl font-bold`}>Hi_Kolade</div>
+      <div className={`flex justify-center my-auto mx-auto`}>Hi_Kolade</div>
     </>
   );
 }
