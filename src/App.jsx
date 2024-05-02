055f0be8ff28fbf490dd4e370187434a8d059868
kolamat <matanmidupe004@gmail.com>
2024-05-02 21:12:24 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
-import nancy from "../public/image.png";
+import nancy from "/image.png";
+import Type from "typed-js";
+
+class User extends Type {
+  constructor(name, password) {
+    super({
+      username: Type.STRING,
+      password: Type.STRING,
+    });
+    this.username = name;
+    this.password = password;
+  }
+  getUserName() {
+    return this.username;
+  }
+  setUserName(kolade) {
+    this.username = kolade;
+  }
+}
+
+console.log(constructor);
 
 function App() {
   return (
